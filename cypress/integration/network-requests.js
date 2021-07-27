2d0d9ccaab044a42123709022ea8c6872e351151
Marcin <marcinkufel84@gmail.com>
2021-07-27 07:37:00 +0200

    added test to GET response

---
+++ cypress/integration/network-requests.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+describe("Network Requests", () => {
+    beforeEach(() => {
+        cy.visit("https://example.cypress.io/commands/network-requests")
+
+//Start a server to begin routing responses to cy.route() and cy.request().
+        cy.server();
+    })
+
+    it ("Get Request", () => {
+        //Listen to GET requests which use the following: comment/ within the url
+        //cy.route("GET", "comments/*").as("getComment");
+
+        cy.get(".network-btn").click();
+
+        cy.wait("@getComment").its("status").should("eq", 200)
+    })
+})
